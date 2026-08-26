f8989e87d147b3c491994b32ae1ee7414c85f3e9
byungsa <50562790+byungsa@users.noreply.github.com>
2026-08-26 11:18:40 +0000

    complete day03

---
+++ day03.py
@@ -0,0 +1,18 @@
+
+for i in range(1,11):
+    print (i)
+
+
+scores = [95, 70, 85, 60, 100]
+
+#95점 : 합격
+#70점 : 불합격
+#85점 : 합격
+#60점 : 불합격
+#100점 : 합격
+
+for score in scores:
+    if score >= 80:
+     print("합격")
+    else:
+     print("불합격")
